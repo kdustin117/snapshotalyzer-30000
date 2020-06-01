dddd102537396f1f2b4ac59df05feaa0e1599f1b
kdustin117 <43281203+kdustin117@users.noreply.github.com>
2020-06-01 16:39:57 -0400

    uploading shotty with click

---
+++ shotty.py
@@ -0,0 +1,27 @@
+# ^ shift K to Kill a line
+# OPTION LEFT/RIGHT ARROW for move one word
+# OPTION DELETE deletes word to left of cursor
+
+import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
+
+
+if __name__ == '__main__':
+    print()
+    list_instances()
